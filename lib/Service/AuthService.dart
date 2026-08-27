f73e82aa3c856c061a42f6c38d101e4a1d6a6e04
Darshan Lonkar <darshanlonkar@allmiles.in>
2026-08-27 13:16:42 +0530

    Fix testability: lazy Firebase init in AuthService

--- lib/Service/AuthService.dart
+++ lib/Service/AuthService.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
 
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  late final FirebaseAuth _auth = FirebaseAuth.instance;
 
 
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:dio/dio.dart';
 
 import 'package:employeeapp/main.dart';
@@ -13,41 +11,6 @@ import 'package:employeeapp/repository/auth_repository..dart';
 import 'package:employeeapp/repository/EmployeeRepository.dart';
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUpAll(() async {
-    const MethodChannel channel =
-    MethodChannel('plugins.flutter.io/firebase_core');
-
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-        .setMockMethodCallHandler(channel, (MethodCall methodCall) async {
-      if (methodCall.method == 'Firebase#initializeCore') {
-        return [
-          {
-            'name': '[DEFAULT]',
-            'options': {
-              'apiKey': 'fake-api-key',
-              'appId': 'fake-app-id',
-              'messagingSenderId': 'fake-sender-id',
-              'projectId': 'fake-project-id',
-            },
-            'pluginConstants': {},
-          }
-        ];
-      }
-      if (methodCall.method == 'Firebase#initializeApp') {
-        return {
-          'name': methodCall.arguments['appName'],
-          'options': methodCall.arguments['options'],
-          'pluginConstants': {},
-        };
-      }
-      return null;
-    });
-
-    await Firebase.initializeApp();
-  });
-
   testWidgets('Employee Management App loads successfully',
           (WidgetTester tester) async {
         await tester.pumpWidget(
